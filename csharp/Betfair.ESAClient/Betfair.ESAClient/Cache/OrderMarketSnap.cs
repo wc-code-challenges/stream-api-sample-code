5798bfa59da8cf0af705a3bff01e6f480eccde7d
betvader <bet.vader@yahoo.com>
2020-08-25 11:46:25 +1000

    fix: using statements

--- csharp/Betfair.ESAClient/Betfair.ESAClient/Cache/OrderMarketSnap.cs
+++ csharp/Betfair.ESAClient/Betfair.ESAClient/Cache/OrderMarketSnap.cs
@@ -1,18 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Betfair.ESAClient.Cache
 {
-    /// <summary>
-    /// Thread safe atomic snapshot of a market.
-    /// Reference only changes if the snapshot changes:
-    /// i.e. if snap1 == snap2 then they are the same.
-    /// (same is true for sub-objects)
-    /// </summary>
-    public class OrderMarketSnap
+	/// <summary>
+	/// Thread safe atomic snapshot of a market.
+	/// Reference only changes if the snapshot changes:
+	/// i.e. if snap1 == snap2 then they are the same.
+	/// (same is true for sub-objects)
+	/// </summary>
+	public class OrderMarketSnap
     {
         public string MarketId { get; internal set; }
         public bool IsClosed { get; internal set; }
